f5e4785ee2e899cabc25627b5d4a2f1cc75f0f5b
EujeSol <62849387+EujeSol@users.noreply.github.com>
2021-05-11 21:40:56 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LR11';
+  notes: Note[] = [
+    {id: 1, date: new Date, name: 'Note', text: 'Note Note Note'},
+    {id: 2, date: new Date, name: 'Note1', text: 'Note1 Note1 Note1'}
+  ];
+
+  add_title: string;
+  add_text: string;
+
+  onAddNote ()
+  {
+    let note: Note = 
+    {id: this.notes.length + 1, date: new Date, name: this.add_title, text: this.add_text}
+    this.notes.push(note);
+    this.add_title = "";
+    this.add_text = '';
+  }
+
+  onDeleteNote(id: number)
+  {
+    let index = this.notes.findIndex( (note) => note.id === id);
+    if(index !== -1)
+    this.notes.splice(index, 1);
+    
+  }
+
+}
+
+interface Note
+{
+  id: number;
+  date: Date;
+  name: string;
+  text: string;
+}
+
